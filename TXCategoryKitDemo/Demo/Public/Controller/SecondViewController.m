ac160a5224d536ee7b1386323b5d34b33d78471f
chenxiaojie <chenjie003@qq.com>
2022-03-03 17:04:39 +0800

    add TXCategoryKitDemo

---
+++ TXCategoryKitDemo/Demo/Public/Controller/SecondViewController.m
@@ -0,0 +1,78 @@
+//
+//  SecondViewController.m
+//  Demo
+//
+//  Created by ChenJie on 2017/12/12.
+//  Copyright © 2017年 ChenJie. All rights reserved.
+//
+
+#import "SecondViewController.h"
+#import "TXCategoryKit.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *identifier = @"UITableViewCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+//    if (indexPath.row == 0) {
+//        cell.backgroundColor = [UIColor colorWithHexString:@"FFF000FF"];
+//    } else if (indexPath.row == 1) {
+//        cell.backgroundColor = [UIColor colorWithHexString:@"FFF000E0"];
+//    } else if (indexPath.row == 2) {
+//        cell.backgroundColor = [UIColor colorWithHexString:@"FFF000C2"];
+//    } else if (indexPath.row == 3) {
+//        cell.backgroundColor = [UIColor colorWithHexString:@"FFF00040"];
+//    } else if (indexPath.row == 4) {
+//        cell.backgroundColor = [UIColor colorWithHexString:@"FFF00000"];
+//    } else if (indexPath.row == 5) {
+//        cell.backgroundColor = [UIColor colorWithHexString:@"FFF000"];
+//    }
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 60;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
